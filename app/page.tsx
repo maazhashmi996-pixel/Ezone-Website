d677193fbac107e14b0ba2f1f74158840dfe9d3a
CodeJaveria <javeriasharif145@gmail.com>
2026-03-09 20:38:01 +0500

    links created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Hero from "./components/ui/Hero";
+import Hero from "./components/sections/Hero";
+import UniversitiesSection from "./components/sections/UniversitiesSection";
+import StudyDestinations from "./components/sections/StudyDestinations";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <StudyDestinations/>
+      <UniversitiesSection />
     </main>
   );
 }
